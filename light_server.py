ff0b1b0fc75a5c3c78e2dddaeeb9009800d31ad6
shaked <shakedownez@gmail.com>
2018-12-01 00:02:46 +0200

    Added the ability for the server to get specific messages: 	On port 1340 the server listens to one of the following: 	TOGGLE: Toggles the state of the light, turning it on/off and returning TOGGLED 	STATE: Checks the state in the file light_state and returns 0 for off and 1 for on 	Any other should make the server return BAD_REQUEST (all are strings) The state of the light is stored in a file for later access

--- light_server.py
+++ light_server.py
@@ -1,22 +1,80 @@
 # light_server.py
 
 import socket
-import RPi.GPIO as gpio
+# import RPi.GPIO as gpio
 import datetime
 import sys
 import traceback
 
-power = False
+power = None
 pin_number = 2
-addr = ("10.100.102.44", 4444)
+# addr = ("10.100.102.44", 4444)
+addr = ("127.0.0.1", 5555)
 debug_mode = True
 
-def toggle():
-	power = not power
+# Files
+__light_state = "./light_state"
+__log_filename = "./log/server_log"
+
+
+# Responses
+RESPONSE_TOGGLE = "TOGGLED"
+RESPONSE_STATE_ON = "1"
+RESPONSE_STATE_OFF = "0"
+RESPONSE_BAD_REQUEST = "BAD_REQUEST"
+
+
+def file_write(msg, filename):
+	"""
+	Logs a message to a desired file, fails if one
+	of the arguments is not specified
+	"""
+	f = open(filename, "a+")
+	d = datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
+	f.write(str(d) + ":")
+	f.write(msg)
+	f.write("\n\n")
+	f.close()
+
+
+def debug(msg):
 	if debug_mode:
+		file_write(msg, __log_filename)
+
+
+def get_state():
+	stat_file = open(__light_state, "r")
+	ans = stat_file.read(1)  # Read 1 character, 0 or 1
+	stat_file.close()
+	# debug("read status: " + str(ans))
+	return str(ans)
+
+def set_state(stat):
+	stat_file = open(__light_state, "w+")
+	stat_file.write(stat)
+	stat_file.close()
+	# debug("wrote to file: " + str(stat))
+
+
+def toggle():
+	current_status = get_state()
+	if current_status == "1":
+		power = False  # if power was "1" (on) change it to false (off)
+		set_state("0")
+	elif current_status == "0":
+		power = True
+		set_state("1")
+	else:
+		log("ERROR! Read unexpected value from the state file at: " + __light_state + ": " + current_status)
+
+
+	# remove this on live
+	if not debug_mode:
 		gpio.output(pin_number, power);
 	else:
 		print "toggle() called, changed power to %s" % (str(power))
+
+
 
 
 def setup():
@@ -26,32 +84,36 @@ def setup():
 	else:
 		print "setup() called"
 
-def log(msg):
-	f = open("log.txt", "a+")
-    d = datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
-    f.write(str(d) + ":")
-    f.write(msg)
-    f.write("\n\n")
-    f.close()
+	set_state("0")  # first, set it to OFF
+
+
 
 
 def handle_request(client_socket):
 	try:
-		# print "toggled %s " % str(power)
-
 		msg = client_socket.recv(1024)
 		print "got:", msg
 
-		toggle()  # Perform a toggle to the light
-		client_socket.send(("Power: " + str(power)).encode())
-		client.close()
+		ans = RESPONSE_BAD_REQUEST  # Default is bad request
+		if msg.startswith("STATE"):
+			ans = get_state()
+			debug("status requested... answering with: " + ans)
+		elif msg.startswith("TOGGLE"):
+			toggle()  # Perform a toggle to the light
+			ans = RESPONSE_TOGGLE
+			debug("toggle requested... state changed to: " + ans)
+		else:
+			debug("Unexpected request: " + str(msg))
+
+		client_socket.send(ans.encode())
+		client_socket.close()
 	except:
-		log("ERROR OCCURED")
+		log("FATAL ERROR OCCURED")
 		log(str(sys.exc_info()))
 		log(traceback.format_exc())
 		client.close()
 
-if __name__ == "main":
+if __name__ == "__main__":
 	# Initial setup
 	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server.bind(addr)
@@ -59,7 +121,8 @@ if __name__ == "main":
 	setup()
 
 	while 1:
-		print "Waiting on incoming connections..."
+		print "Waiting on incoming connections on: ", addr
 		(client, c_addr) = server.accept()
+		print "Accepted connection"
 		handle_request(client) # TODO: Do that on a different thread
 
